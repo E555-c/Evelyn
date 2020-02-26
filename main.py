a230a2bb3ef79c537fc70cf1f27d7ec89f4bf79b
E555-c <evepacheco419@gmail.com>
2020-02-26 03:58:38 +0000

    Cree un nuevo codigo

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Musica favorita de Evelyn")
+print()
+print("1.- Morat")
+print("2.- Cnco")
+opc = int(input("Ingrese un  numero: "))
+if (opc == 1):
+  print("Morat")
+  can = input("Ingrese una cancion de Morat: ")
+elif (opc == 2):
+  print("Cnco")
+  can = input("Ingrese una cancion de Cnco: ")
